1fbb5ec8f298fa974c7fdde3d21fefda2468a11b
Tibor Krajčovič <dev.tiqra@gmail.com>
2023-08-27 16:01:05 +0200

    sort messages by timestamp descending

--- src/messages/message.service.ts
+++ src/messages/message.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { FilterQuery, Model, ProjectionType } from 'mongoose';
+import { FilterQuery, Model, ProjectionType, SortOrder } from 'mongoose';
 import { SELECT_MESSAGES_PROJECTION } from './message.constants';
 import { IMessage } from './message.interface';
 import { IMessageService } from './message.service.interface';
@@ -31,8 +31,9 @@ export class MessageService implements IMessageService, OnApplicationBootstrap {
   find(
     filter: FilterQuery<IMessage>,
     projection?: ProjectionType<any>,
+    sort?: string | { [key: string]: SortOrder | { $meta: any } } | [string, SortOrder][] | undefined | null,
   ): Promise<Array<IMessage>> {
-    return this.repository.find(filter, projection);
+    return this.repository.find(filter, projection, sort);
   }
 
   findById(id: string): Promise<IMessage> {
